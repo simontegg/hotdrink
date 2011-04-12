5851a735ae6aa81469d7965cc0906ee84d062c93
Wonseok Kim <guruwons@tamu.edu>
2011-04-12 18:30:01 -0500

    (split) Update survey form example to use Eve layout.

--- test/examples/surveyform.php
+++ test/examples/surveyform.php
@@ -1,37 +1,39 @@
 <?php
-// enablement/disablement example
+// enablement/disablement of widgets
+// command activation based on invariants
 
 $sheet = <<<EOS
 sheet survey {
 
   interface: {
-    q1;
-    q2;
+    q1: empty; //"true" or "false" TODO: --> boolean value
+    q2; //string
     q2_other: "";
 
-    q3: [];
-    q3_other : false;
-    q3_other_text: "";
+    q3: []; //array of string
+    q3_other : "";
     
-    q4_flag : false;
-    q4_reason;
+    q4 : false;
+    q4_reason : "";
   }
   
   logic: {
+    a1 <== (q1 == "true") ? true : false;
+    a2 <== (q1 == empty || q1 == "true") ? empty : {code: q2, other: (q2 == "other") ? q2_other : empty};
+    a3 <== (q1 == empty || q1 == "false") ? empty : {list: q3, other: contains(q3, "other") ? q3_other : empty};
+    a4 <== (q1 == empty || q1 == "false") ? empty : (q4 ? q4_reason : empty);
   }
 
   output: {
-    a1 <== q1;
-    a2 <== (q1 == "true") ? empty : (q2 == "other") ? q2_other : q2;
-    a3 <== (q1 == "false") ? empty : ((q3_other == false) ? q3 : q3.concat(q3_other_text));
-    a4 <== (q1 == "true" && q4_flag == true) ? q4_reason : empty;
-	
-    result <== {answer1: a1, answer2: a2, answer3: a3, answer4: a4};
+    result <== {a1: a1, a2: a2, a3: a3, a4: a4};
   }
   
   invariant: {
-    // OK is enabled if q1 is Yes or (q1 is No and q2 is answered)
-    must_be <== q1 != empty && ( q1 == "true" || (q2 != empty && (q2 != "other" || q2_other != "")));
+    // invariants are used to validate the form values
+    // for demonstration, the result will be used deactivate submit button
+    invariant1 <== q1 != empty;
+    invariant2 <== q1 == "true" || (q2 != empty && (q2 != "other" || q2_other != ""));
+    invariant4 <== !q4 || q4_reason != "";
   }
 }
 
@@ -39,56 +41,78 @@ EOS;
 
 
 $layout = <<<EOS
-
-<style type="text/css">
-#dialog { border: 1px solid black; padding: 1em; margin: 1em; }
-#dialog label { display: block; }
-</style>
-
-<form id="dialog">
-<label>1. Are you using online social networking services?</label>
-<input type="radio" name="q1" id="q1" value="true">Yes</input>
-<input type="radio" name="q1" value="false">No</input>
-<br>
-
-<br>
-
-<label>2. If you answered No for the question 1, what is your reason?</label>
-<input type="radio" name="q2" id="q2" value="privacy">Privacy Concern</input><br>
-<input type="radio" name="q2" value="not_effective">Not effective for networking</input><br>
-<input type="radio" name="q2" value="not_necessary">Not necessary</input><br>
-<input type="radio" name="q2" value="other">Other</input>
-<input type="text" id="q2_other"/><br>
-
-<br>
-
-<label>3. Which online social networking services are you using?</label>
-
-<!-- binding multi-checkbox to a model variable -->
-<input type="checkbox" name="q3" id="q3" value="Facebook">Facebook</input><br>
-<input type="checkbox" name="q3" value="Twitter">Twitter</input><br>
-<input type="checkbox" name="q3" value="MySpace">MySpace</input><br>
-<input type="checkbox" name="q3" value="LinkedIn">LinkedIn</input><br>
-
-<input type="checkbox" id="q3_other" value="Other">Other</input>
-<input type="text" id="q3_other_text"/><br>
-<br>
-
-<label>4. Do you use Facebook? If yes, what is your favorite feature of Facebook?</label>
-<input type="checkbox" id="q4_flag">Yes</input> 
-<select id="q4_reason">
-  <option value="">Choose</option>
-  <option value="searching">Searching Friends</option>
-  <option value="pictures">Sharing Pictures</option>
-  <option value="status">Sharing Status</option>
-  <option value="chat">Chatting</option>
-</select>
-
-<br>
-<br>
-
-<button type="button" id="result">OK</button>
-</form>
+view {
+  row {
+    text (readonly, value : "1. Are you using online social networking services?");
+  }
+  row {
+    radioGroup (
+      items : [
+        { name : "Yes", value : "true" },
+        { name : "No", value : "false" }
+      ],
+      value : q1
+    );
+  }
+  
+  row {
+    text (readonly, value : "2. If you answered No for the question 1, what is your reason?");
+  }
+  row {
+    radioGroup (
+      items : [
+        { name : "Privacy Concern", value : "privacy" },
+        { name : "Not effective for networking", value : "not_effective" },
+        { name : "Not necessary to me", value : "not_necessary"},
+        { name : "Other (please specify)", value : "other"}
+      ],
+      value : q2
+    );
+  }
+  row {
+    //TODO put this after Other button
+    text (value : q2_other);
+  }
+  
+  row {
+    text (readonly, value : "3. Which online social networking services are you using?");
+  }
+  row {
+    checkboxGroup (
+    	items : [
+    	  { name : "Facebook", value : "facebook" },
+    	  { name : "Twitter", value : "twitter" },
+    	  { name : "MySpace", value : "myspace" },
+    	  { name : "LinkedIn", value : "linkedin" },
+    	  { name : "Other", value : "other" }
+    	],
+    	value : q3
+    );
+  }
+  row {
+    text (value : q3_other);
+  }
+  
+  row {
+    text (readonly, value : "4. Do you use Facebook? If yes, what is your favorite feature of Facebook?");
+  }
+  row {
+    //TODO put label "Yes" after the checkbox
+    checkbox (name : "Yes", value : q4);
+    dropdown (
+      items : [
+        { name : "Choose One", value : ""},
+        { name : "Searching Friends", value : "search"},
+        { name : "Sharing Pictures", value : "picture"},
+        { name : "Sharing Status", value : "status"},
+        { name : "Chatting", value : "chat"}
+      ],
+      value : q4_reason
+    );
+  }
+  
+  commandButton(label : "Submit", value : result);
+}
 EOS;
 
 ?>
