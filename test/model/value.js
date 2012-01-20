7482321f79e4e9baaf37c92eb5d0c1383b996b46
John Freeman <jfreeman@cse.tamu.edu>
2012-01-20 14:39:43 -0600

    (split) more tests and a functionality fix - we will need to call binding callbacks on bind rather than expecting an update after binding

--- test/model/value.js
+++ test/model/value.js
@@ -1,17 +1,36 @@
 (function () {
 
-  module("value propagation");
+  module("model");
 
   test("resize image", function () {
     var model = hotdrink.makeModelController({
       cgraph : hottest.resize_image.cgraph,
       methods : hottest.resize_image.methods
     });
+    equal(model.get("initial_width"), 2100,
+      "initialized with constant numeric expression");
+    equal(model.get("absolute_height"), 1500,
+      "initialized with variable numeric expression");
+    equal(model.get("preserve_ratio"), true,
+      "initialized with constant boolean expression");
+    equal(model.get("relative_width"), 100,
+      "initialized with evaluation phase");
 
     model.set("relative_width", 105);
     model.update();
-    
-    equal(model.get("relative_height"), 105);
+    equal(model.get("absolute_height"), 1575,
+      "value propagation");
+
+    model.set("preserve_ratio", false);
+    model.update();
+    equal(model.get("relative_height"), 105,
+      "self-loop");
+
+    model.set("relative_height", 100);
+    model.set("preserve_ratio", true);
+    model.update();
+    equal(model.get("relative_width"), 100,
+      "preservation of more recently edited value");
   });
 
 }());
