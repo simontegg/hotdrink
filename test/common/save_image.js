22431cced6ba103e2a2b43409eb8c4325c2bdc90
John Freeman <jfreeman@cse.tamu.edu>
2012-01-23 17:42:22 -0600

    (split) enablement test; save_image

---
+++ test/common/save_image.js
@@ -0,0 +1,73 @@
+(function () {
+
+  var cgraph = {
+    variables : {
+      file_name : {
+        "cellType" : "interface",
+        "usedBy" : ["__method_3","__method_4"],
+        "initializer" : "\"\""
+      },
+      file_type : {
+        "cellType" : "interface",
+        "usedBy" : ["__method_3"],
+        "initializer" : "\"bmp\""
+      },
+      compression_ratio : {
+        "cellType" : "interface",
+        "usedBy" : ["__method_2","__method_3"],
+        "initializer" : "100"
+      },
+      image_quality : {
+        "cellType" : "interface",
+        "usedBy" : ["__method_1"],
+        "initializer" : "100"
+      },
+      result : {
+        "cellType" : "output",
+        "usedBy" : []
+      },
+      check_name : {
+        "cellType" : "invariant",
+        "usedBy" : []
+      }
+    },
+    methods : {
+      __method_1 : {
+        "inputs" : ["image_quality"],
+        "outputs" : ["compression_ratio"]
+      },
+      __method_2 : {
+        "inputs" : ["compression_ratio"],
+        "outputs" : ["image_quality"]
+      },
+      __method_3 : {
+        "inputs" : ["file_type","file_name","compression_ratio"],
+        "outputs" : ["result"]
+      },
+      __method_4 : {
+        "inputs" : ["file_name"],
+        "outputs" : ["check_name"]
+      }
+    },
+    constraints : {
+      __constraint_1 : { "methods" : ["__method_1","__method_2"] },
+      __constraint_2 : { "methods" : ["__method_3"] },
+      __constraint_3 : { "methods" : ["__method_4"] }
+    }
+  };
+
+  var methods = "{ __method_1 : function(E) {return (100-(4*(100-E.eval1(\"image_quality\"))));}, __method_2 : function(E) {return (100-((100-E.eval1(\"compression_ratio\"))/4));}, __method_3 : function(E) {return ((E.eval1(\"file_type\")==\"jpeg\") ? {type : E.eval1(\"file_type\"),name : E.eval1(\"file_name\"),ratio : E.eval1(\"compression_ratio\")} : {type : E.eval1(\"file_type\"),name : E.eval1(\"file_name\")});}, __method_4 : function(E) {return (E.eval1(\"file_name\")!=\"\");} }";
+
+  var save_image = {
+    getModel : function () {
+      return hotdrink.makeModelController({
+        cgraph : cgraph,
+        methods : methods
+      });
+    }
+  };
+
+  namespace.open("hottest").save_image = save_image;
+
+}());
+
---
+++ test/model/enablement.js
@@ -0,0 +1,49 @@
+(function () {
+
+  var getCanBeDisabled = function (model, v) {
+    return model.model.behaviors.get("enablement").variables.get(v).canBeDisabled;
+  };
+
+  module("enablement");
+
+  test("save_image", function () {
+    expect(12);
+
+    var model = hottest.save_image.getModel();
+    strictEqual(model.get("file_name"), "",
+      "initialized file_name");
+    strictEqual(model.get("file_type"), "bmp",
+      "initialized file_type");
+    strictEqual(getCanBeDisabled(model, "file_name"), false,
+      "initially enabled");
+    strictEqual(getCanBeDisabled(model, "file_type"), false,
+      "initially enabled");
+    strictEqual(getCanBeDisabled(model, "compression_ratio"), true,
+      "initially disabled");
+    strictEqual(getCanBeDisabled(model, "image_quality"), true,
+      "initially disabled");
+    strictEqual(getCanBeDisabled(model, "result"), true,
+      "initially disabled");
+
+    model.set("file_type", "jpeg");
+    model.update();
+    strictEqual(getCanBeDisabled(model, "compression_ratio"), false,
+      "enabled");
+    strictEqual(getCanBeDisabled(model, "image_quality"), false,
+      "enabled");
+
+    model.set("file_name", "name");
+    model.update();
+    strictEqual(getCanBeDisabled(model, "result"), false,
+      "enabled");
+
+    model.set("file_type", "bmp");
+    model.update();
+    strictEqual(getCanBeDisabled(model, "compression_ratio"), true,
+      "disabled");
+    strictEqual(getCanBeDisabled(model, "image_quality"), true,
+      "disabled");
+  });
+
+}());
+
