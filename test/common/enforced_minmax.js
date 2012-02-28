f28ef20ca9f61b46d686f0761d530b42aacdf240
John Freeman <jfreeman@cse.tamu.edu>
2012-02-28 03:58:40 -0600

    (split) added invariants to model building API

--- test/common/enforced_minmax.js
+++ test/common/enforced_minmax.js
@@ -1,44 +1,36 @@
 var min = function (a, b) { return (a < b) ? (a) : (b); };
 var max = function (a, b) { return (a < b) ? (b) : (a); };
-var __enforced_max = function (value, min_value, max_value) {
-  var t_max_value = max(min_value, max_value);
-  /* Note: t_max_value used here so we don't have to repeat above expression. */
-  var t_value = min(max(value, min_value), t_max_value);
-  return [t_value, t_max_value];
-};
-var __enforced_min = function (value, min_value, max_value) {
-  var t_min_value = min(min_value, max_value);
-  /* Note: t_min_value used here so we don't have to repeat above expression. */
-  var t_value = min(max(value, t_min_value), max_value);
-  return [t_value, t_min_value];
-};
 
 (function () {
 
+  var Model = function () {
+    this.value = hd.variable(50);
+    this.min = hd.variable(0);
+    this.max = hd.variable(100);
+
+    hd.constraint()
+      .bind(["value", "max"], function () {
+        var maxNext = max(this.min(), this.max());
+        var valueNext = min(max(this.min(), this.value()), maxNext);
+        return [valueNext, maxNext];
+      })
+      .bind(["value", "min"], function () {
+        var minNext = min(this.min(), this.max());
+        var valueNext = min(max(minNext, this.value()), this.max());
+        return [valueNext, minNext];
+      });
+
+    this.result = hd.computed(function () {
+      return { value : this.value(), min : this.min(), max : this.max() };
+    });
+
+    hd.invariant(function () { this.min() >= 0; });
+
+  };
+
   var enforced_minmax = {
     getModel : function () {
-      var model = hotdrink.makeModelFactory();
-
-      model.addInterface("value", "50");
-      model.addInterface("min", "0");
-      model.addInterface("max", "100");
-
-      model.addConstraint(
-        model.addMethod(["value", "max"], function (model) {
-          return __enforced_max(model.get("value"),model.get("min"),model.get("max"));
-        }, ["value", "min", "max"]),
-        model.addMethod(["value", "min"], function (model) {
-          return __enforced_min(model.get("value"),model.get("min"),model.get("max"));
-        }, ["value", "min", "max"])
-      );
-
-      model.addOutput("result", function (model) {
-        return {value:model.get("value"),min:model.get("min"),max:model.get("max")};
-      }, ["value", "min", "max"]);
-
-      model.addInvariant("check_min", "model.get(\"min\")>=0", ["min"]);
-
-      return model.close();
+      return hd.model(new Model());
     }
   };
 
